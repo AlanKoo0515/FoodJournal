6fcb1a67a7eaa69270b6ec82d09c77d11b4db423
Alan Koo <alankoo0515@gmail.com>
2025-05-16 01:17:37 +0800

    create module folders and home page for each modules

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\CulinaryExperienceController;
+use App\Http\Controllers\RatingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +18,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Recipe routes
+    Route::resource('recipes', RecipeController::class);
+
+    // Culinary Experience routes
+    Route::resource('experiences', CulinaryExperienceController::class);
+
+    // Rating routes
+    Route::resource('ratings', RatingController::class);
 });
 
 require __DIR__.'/auth.php';
